73170cb58dac448090fc41a0c22b1182f4596a41
MrJoyner95 <oct_romo@hotmail.com>
2019-08-06 12:39:02 -0500

    Bootstrap-Vue.js agregado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
+/**
+ * Boostrap-Vue.js
+**/
+import BootstrapVue from 'bootstrap-vue'
+// Archivos CSS:
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Aplicacion:
+Vue.use(BootstrapVue)
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
